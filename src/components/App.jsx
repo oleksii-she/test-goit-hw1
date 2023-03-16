7b68397aa62fc15745efd15602f877f0a3518e4f
Oleksii Shevchenko <vazerius@gmail.com>
2023-03-16 10:50:31 +0200

    add project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,7 @@
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h2 className=".red">asasasas</h2>
     </div>
   );
 };
